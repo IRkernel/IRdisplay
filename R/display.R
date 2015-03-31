843f7f1fb5edae1194b8aa64ca08dcc3b8ba6576
Philipp A <flying-sheep@web.de>
2015-03-31 10:03:45 +0200

    added svg output, switched to in-memory base64

--- R/display.R
+++ R/display.R
@@ -35,21 +35,40 @@ display_html = function(content) {
 #'
 #' Either data or filename must be passed.
 #' @param data The PNG data as a raw vector
-#' @param filename The path to a PNG file
+#' @param file The path to a PNG file or a connection
 #' @param width The width to display the image
 #' @param height The height to display the image
-#' @importFrom base64 encode
+#' @importFrom base64enc base64encode
 #' @export
-display_png = function(data=NULL, filename=NULL, width=NULL, height=NULL) {
-    if (!is.null(data)) {
-        filename = tempfile()
-        writeBin(data, filename)
-    }
-    tf = tempfile()
-    encode(filename, tf) # base64
-    b64data = paste(readLines(tf), collapse = "")
+display_png = function(data = NULL, file = NULL, width = NULL, height = NULL) {
+    if (!is.null(file)) {
+        b64data <- base64encode(filename)
+    } else if (!is.null(data) && is.raw(data)) {
+        b64data <- base64encode(data)
+    } else stop('Either need to specify raw data as vector or filename/connection')
+    
     metadata = setNames(list(), character(0))
-    if (!is.null(width)) metadata$width=width
-    if (!is.null(height)) metadata$height=height
+    if (!is.null(width))  metadata$width  <- width
+    if (!is.null(height)) metadata$height <- height
     display1('image/png', b64data, metadata)
+}
+
+#'Display SVG output
+#'
+#' Either data or filename must be passed.
+#' @param data The SVG data as character vector
+#' @param filename The path to a SVG file
+#' @param width The width to display the image
+#' @param height The height to display the image
+#' @export
+display_svg = function(data = NULL, file = NULL, width = NULL, height = NULL) {
+    if (!is.null(file)) {
+        stopifnot(is.null(data))
+        data <- readChar(file)
+    }
+    
+    metadata = setNames(list(), character(0))
+    if (!is.null(width))  metadata$width  <- width
+    if (!is.null(height)) metadata$height <- height
+    display1('image/svg+xml', data, metadata)
 }
