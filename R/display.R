47f7cc3e0df3c228c4e3f354b516b50c19490059
Phil Schaf <flying-sheep@web.de>
2015-04-26 15:08:52 +0200

    Refactored display_format functions and added more
    Fixes #5

--- R/display.R
+++ R/display.R
@@ -23,52 +23,120 @@ display1 <- function(mimetype, content, metadata=NULL) {
     base_display(data, metadata)
 }
 
-#'Display HTML output
-#'
-#' @param  content The HTML as a length 1 string vector
-#' @export
-display_html = function(content) {
-    display1('text/html', content)
-}
-
-#'Display PNG output
-#'
-#' Either data or filename must be passed.
-#' @param data The PNG data as a raw vector
-#' @param file The path to a PNG file or a connection
-#' @param width The width to display the image
-#' @param height The height to display the image
-#' @importFrom base64enc base64encode
-#' @export
-display_png = function(data = NULL, file = NULL, width = NULL, height = NULL) {
-    if (!is.null(file)) {
-        b64data <- base64encode(file)
-    } else if (!is.null(data) && is.raw(data)) {
-        b64data <- base64encode(data)
-    } else stop('Either need to specify raw data as vector or filename/connection')
+prepare_content <- function(data = NULL, file = NULL, isbinary = TRUE) {
+    if (is.null(file) == is.null(data))
+        stop('Either need to specify data or file, but not both')
     
-    metadata = setNames(list(), character(0))
-    if (!is.null(width))  metadata$width  <- width
-    if (!is.null(height)) metadata$height <- height
-    display1('image/png', b64data, metadata)
-}
-
-#'Display SVG output
-#'
-#' Either data or filename must be passed.
-#' @param data The SVG data as character vector
-#' @param filename The path to a SVG file
-#' @param width The width to display the image
-#' @param height The height to display the image
-#' @export
-display_svg = function(data = NULL, file = NULL, width = NULL, height = NULL) {
-    if (!is.null(file)) {
-        stopifnot(is.null(data))
-        data <- readChar(file, file.info(file)$size)
+    if (isbinary) {
+        if (!is.null(file)) {
+            content <- base64encode(file)
+        } else if (is.raw(data)) {
+            content <- base64encode(data)
+        } else stop('Data needs to be a raw vector')
+    } else {
+        if (!is.null(file)) {
+            content <- readChar(file, file.info(file)$size)
+        } else if (is.character(data)) {
+            content <- data
+        } else stop('Data needs to be a character vector')
     }
     
-    metadata = setNames(list(), character(0))
+    content
+}
+
+display_raw <- function(mimetype, isbinary, data, file, metadata = NULL) {
+    content <- prepare_content(data, file, isbinary)
+    display1(mimetype, content, metadata)
+}
+
+#' Display JSON
+#'
+#' Either data or file must be passed.
+#' @param data  The JSON code as a character vector
+#' @param file  The path to a JSON file or a connection
+#' @export
+display_json <- function(data = NULL, file = NULL) display_raw('application/json', FALSE, data, file)
+
+#' Display (execute) Javascript
+#'
+#' Either data or file must be passed.
+#' @param data  The Javascript code as a character vector
+#' @param file  The path to a Javascript file or a connection
+#' @export
+display_javascript <- function(data = NULL, file = NULL) display_raw('application/javascript', FALSE, data, file)
+
+#' Display HTML output
+#'
+#' Either data or file must be passed.
+#' @param data  The HTML code as a character vector
+#' @param file  The path to a HTML file or a connection
+#' @export
+display_html <- function(data = NULL, file = NULL) display_raw('text/html', FALSE, data, file)
+
+#' Display Markdown output
+#'
+#' Either data or file must be passed.
+#' @param data  The Markdown code as a character vector
+#' @param file  The path to a Markdown file or a connection
+#' @export
+display_markdown <- function(data = NULL, file = NULL) display_raw('text/markdown', FALSE, data, file)
+
+#' Display LaTeX output
+#'
+#' Either data or file must be passed.
+#' @param data  The LaTeX code as a character vector
+#' @param file  The path to a LaTeX file or a connection
+#' @export
+display_latex <- function(data = NULL, file = NULL) display_raw('text/latex', FALSE, data, file)
+
+#' Display PNG output
+#'
+#' Either data or file must be passed.
+#' @param data  The PNG data as a raw vector
+#' @param file  The path to a PNG file or a connection
+#' @param width  The width to display the image
+#' @param height  The height to display the image
+#' 
+#' @export
+display_png <- function(data = NULL, file = NULL, width = NULL, height = NULL) {
+    metadata = namedlist()
     if (!is.null(width))  metadata$width  <- width
     if (!is.null(height)) metadata$height <- height
-    display1('image/svg+xml', data, metadata)
+    
+    display_raw('image/png', TRUE, data, file, metadata)
 }
+
+#' Display JPEG output
+#'
+#' Either data or file must be passed.
+#' @param data  The JPEG data as a raw vector
+#' @param file  The path to a JPEG file or a connection
+#' @param width  The width to display the image
+#' @param height  The height to display the image
+#' 
+#' @export
+display_jpeg <- function(data = NULL, file = NULL, width = NULL, height = NULL) {
+    metadata = namedlist()
+    if (!is.null(width))  metadata$width  <- width
+    if (!is.null(height)) metadata$height <- height
+    
+    display_raw('image/jpeg', TRUE, data, file, metadata)
+}
+
+#' Display PDF output
+#'
+#' Either data or file must be passed.
+#' @param data  The PDF data as a raw vector
+#' @param file  The path to a PDF file or a connection
+#' 
+#' @export
+display_pdf <- function(data = NULL, file = NULL) display_raw('application/pdf', TRUE, data, file)
+
+#' Display SVG output
+#'
+#' Either data or file must be passed.
+#' @param data  The SVG data as character vector
+#' @param filename  The path to a SVG file
+#' 
+#' @export
+display_svg <- function(data = NULL, file = NULL) display_raw('image/svg+xml', FALSE, data, file)
